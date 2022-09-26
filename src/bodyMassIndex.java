82eedd0e055aaf126ec9c9b0607f4bf2a846766e
cemssari <cemsari7@gmail.com>
2022-09-26 15:13:48 +0300

    tamamlandi.

---
+++ src/bodyMassIndex.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bodyMassIndex {
+    public static void main(String[] args) {
+        double boy, kilo, bodyMassIndex;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen Boyunuzu (metre cinsinde) Giriniz: ");
+        boy = input.nextDouble();
+
+        System.out.print("Lütfen Kilonuzu Giriniz: ");
+        kilo = input.nextDouble();
+
+        bodyMassIndex = kilo/(boy*boy);
+        System.out.print("Vücut Kitle İndeksiniz : " + bodyMassIndex);
+
+
+
+
+
+    }
+
+}
